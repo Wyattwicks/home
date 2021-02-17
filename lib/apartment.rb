6f9ddf1bd3aafbf4003fbaaaf69bdeec98d9d917
Wyatt <wyattwicks@gmail.com>
2021-02-17 09:13:45 -0700

    Add apartment, renter, and test files for iteration 1

---
+++ lib/apartment.rb
@@ -0,0 +1,16 @@
+class Apartment
+
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+
+  def initialize(unit)
+    @number = unit[:number]
+    @monthly_rent = unit[:monthly_rent]
+    @bathrooms = unit[:bathrooms]
+    @bedrooms = unit[:bedrooms]
+
+  end
+
+  def add_renter(renter)
+    @renter = renter
+  end
+end
