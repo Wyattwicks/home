411dfab1774c8c284c301a13846eec3bceb1a44b
Wyatt <wyattwicks@gmail.com>
2021-02-17 10:55:39 -0700

    Add answers for iteration 3

--- lib/building.rb
+++ lib/building.rb
@@ -4,7 +4,7 @@ require './lib/renter'
 
 class Building
 
-  attr_reader :units, :renters
+  attr_reader :units, :renters, :bedrooms, :number
 
   def initialize
     @units = []
@@ -30,7 +30,41 @@ class Building
        @average_rent << unit.monthly_rent
     end
     @average_rent.inject{|sum, el| sum + el }.to_f / @average_rent.size
-
   end
+
+  def rented_units
+    @rented_units = []
+    @units.find_all do |unit|
+      if unit.renter == nil
+        nil
+      else
+        @rented_units << unit
+      end
+    end
+  end
+
+  def renter_with_highest_rent
+
+    highest_rent = []
+    @rented_units.max_by do |unit|
+      highest_rent << unit.renter
+    end
+    highest_rent[0]
+  end
+
+  def units_by_number_of_bedrooms
+    bedroom_hash = Hash.new
+    @units.max_by do |unit|
+        
+      bedroom_hash.each do |bedrooms:, number:|
+      end
+    end
+    bedroom_hash.max_by do |bedrooms:|
+    end
+    bedroom_hash
+  end
+
+
+
 
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -6,6 +6,13 @@ require './lib/renter'
 
 class BuildingTest < Minitest::Test
 
+  def setup
+    @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @unit3 = Apartment.new({number: "C3", monthly_rent: 1150, bathrooms: 2, bedrooms: 2})
+    @unit4 = Apartment.new({number: "D4", monthly_rent: 1500, bathrooms: 2, bedrooms: 3})
+  end
   def test_new_building
     building = Building.new
 
@@ -34,7 +41,6 @@ class BuildingTest < Minitest::Test
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     renter1 = Renter.new("Aurora")
     renter2 = Renter.new("Tim")
-
     unit1.add_renter(renter1)
     unit2.add_renter(renter2)
     building.add_unit(unit1)
@@ -49,6 +55,48 @@ class BuildingTest < Minitest::Test
     unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
     building.add_unit(unit1)
     building.add_unit(unit2)
+
     assert_equal 1099.5, building.average_rent
   end
+
+  def test_rented_units
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    renter1 = Renter.new("Spencer")
+    @unit2.add_renter(renter1)
+
+    assert_equal [@unit2], @building.rented_units
+  end
+
+  def test_renter_with_highest_rent
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+    renter1 = Renter.new("Spencer")
+    @unit2.add_renter(renter1)
+    renter2 = Renter.new("Jessie")
+    @unit1.add_renter(renter2)
+    renter3 = Renter.new("Max")
+    @unit3.add_renter(renter3)
+
+
+    @building.rented_units
+    assert_equal renter2, @building.renter_with_highest_rent
+  end
+
+  def test_units_by_number_of_bedrooms
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+    @building.add_unit(@unit3)
+
+    bedroom_list = {
+                    3 => ["D4" ],
+                    2 => ["B2", "C3"],
+                    1 => ["A1"]
+                    }
+
+    assert_equal bedroom_list, @building.units_by_number_of_bedrooms
+  end
+
 end
